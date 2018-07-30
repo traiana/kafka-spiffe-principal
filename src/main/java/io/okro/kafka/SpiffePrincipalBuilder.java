f1da6e2a6562e41210c2e37072a6908f726a51fa
Roy Antman <roy.antman@gmail.com>
2018-07-30 09:17:17 +0300

    * Add a fallback to Subject principal when SAN principal could not be determined. * Add logging for debug

--- src/main/java/io/okro/kafka/SpiffePrincipalBuilder.java
+++ src/main/java/io/okro/kafka/SpiffePrincipalBuilder.java
@@ -7,10 +7,19 @@ import javax.net.ssl.SSLSession;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateParsingException;
 import java.security.cert.X509Certificate;
+import java.util.Collection;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.apache.commons.lang3.StringUtils.startsWith;
 
 public class SpiffePrincipalBuilder implements KafkaPrincipalBuilder {
+    private static final Logger LOG = LoggerFactory.getLogger(SpiffePrincipalBuilder.class);
+
+    private static final String SPIFFE_TYPE = "SPIFFE";
+
     public KafkaPrincipal build(AuthenticationContext context) {
         if (context instanceof PlaintextAuthenticationContext) {
             return KafkaPrincipal.ANONYMOUS;
@@ -30,13 +39,26 @@ public class SpiffePrincipalBuilder implements KafkaPrincipalBuilder {
                 return KafkaPrincipal.ANONYMOUS;
             }
             X509Certificate cert = (X509Certificate) peerCerts[0];
-            return cert.getSubjectAlternativeNames().stream()
-                    .map(san -> (String) san.get(1))
-                    .filter(uri -> startsWith(uri, "spiffe://"))
-                    .findFirst()
-                    .map(s -> new KafkaPrincipal(KafkaPrincipal.USER_TYPE, s))
-                    .orElse(KafkaPrincipal.ANONYMOUS);
+
+            Collection<List<?>> sanCollection = cert.getSubjectAlternativeNames();
+            KafkaPrincipal principal;
+
+            if (sanCollection != null) {
+                principal = sanCollection.stream()
+                        .map(san -> (String) san.get(1))
+                        .filter(uri -> startsWith(uri, "spiffe://"))
+                        .findFirst()
+                        .map(s -> new KafkaPrincipal(SPIFFE_TYPE, s))
+                        .orElse(new KafkaPrincipal(KafkaPrincipal.USER_TYPE, cert.getSubjectX500Principal().getName()));
+            } else {
+                principal = new KafkaPrincipal(KafkaPrincipal.USER_TYPE, cert.getSubjectX500Principal().getName());
+            }
+
+            LOG.debug("PrincipalBuilder found principal: {}", principal.toString());
+
+            return principal;
         } catch (SSLPeerUnverifiedException | CertificateParsingException se) {
+            LOG.warn("Unhandled exception: " + se.toString());
             return KafkaPrincipal.ANONYMOUS;
         }
     }
